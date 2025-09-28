dbbeb4771919ef790216eb5f75ef03a9f34cbe37
Kiliangil88 <149784768+Kiliangil88@users.noreply.github.com>
2025-09-28 21:30:21 +0000

    5 primeros ejercicios hechos

---
+++ .learn/resets/05.3-dataframe-head/app.py
@@ -0,0 +1,47 @@
+#print("Hello World")
+import pandas as pd
+data_frame = pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame)
+
+#edades = pd.Series([23, 45, 7, 34, 6, 63, 36, 78, 54, 34])
+#print(edades)
+
+#años = pd.date_range(start='2021-05-01', end='2021-05-12')
+#print(años)
+
+#my_series = pd.Series([2, 4, 6, 8, 10])
+#new_serie = my_series.apply(lambda x: x/2)
+#print(new_serie)
+
+#data = [["Toyota", "Corolla", "Blue"], ["Ford", "K", "Yellow"], ["Porsche", "Cayenne", "White"]]
+#df = pd.DataFrame(data, columns=['Brand','Model','Color'])
+#print(df)
+
+data = [
+    { 
+        "brand": "Toyota", 
+        "model": "Corolla",
+        "color": "Blue"
+    },
+    {
+        "brand": "Ford", 
+        "model": "K",
+        "color": "Yellow"
+    },
+    {
+        "brand": "Porsche", 
+        "model": "Cayenne",
+        "color": "White"
+    },
+    {
+        "brand": "Tesla", 
+        "model": "Model S",
+        "color": "Red"
+    }
+]
+
+df = pd.DataFrame(data)
+print(df)
+
+df = pd.DataFrame(data_frame)
+print(df.iloc[133,6])
---
+++ app.py
@@ -0,0 +1,60 @@
+#print("Hello World")
+import pandas as pd
+#data_frame = pd.read_csv(".learn/assets/pokemon_data.csv")
+#print(data_frame)
+
+#edades = pd.Series([23, 45, 7, 34, 6, 63, 36, 78, 54, 34])
+#print(edades)
+
+#años = pd.date_range(start='2021-05-01', end='2021-05-12')
+#print(años)
+
+#my_series = pd.Series([2, 4, 6, 8, 10])
+#new_serie = my_series.apply(lambda x: x/2)
+#print(new_serie)
+
+#data = [["Toyota", "Corolla", "Blue"], ["Ford", "K", "Yellow"], ["Porsche", "Cayenne", "White"]]
+#df = pd.DataFrame(data, columns=['Brand','Model','Color'])
+#print(df)
+
+data = [
+    { 
+        "brand": "Toyota", 
+        "model": "Corolla",
+        "color": "Blue"
+    },
+    {
+        "brand": "Ford", 
+        "model": "K",
+        "color": "Yellow"
+    },
+    {
+        "brand": "Porsche", 
+        "model": "Cayenne",
+        "color": "White"
+    },
+    {
+        "brand": "Tesla", 
+        "model": "Model S",
+        "color": "Red"
+    }
+]
+
+#df = pd.DataFrame(data)
+#print(df)
+
+#df = pd.DataFrame(data_frame)
+#print(df.iloc[133,6])
+
+#print(df.head(3))
+
+#print(df.tail(3))
+
+#print(df[['Name','Type 1']][0:10])
+
+#print(df.loc[df['Attack']>80])
+
+#print(len(df.loc[df['Legendary'] == True]))
+
+baby_names = pd.read_csv(".learn/assets/us_baby_names_right.csv")
+print(baby_names.head())
